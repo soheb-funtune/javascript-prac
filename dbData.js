1a2357a7d19203ceb9a71bb6bddf85cee635739e
SayyadSoheb1999 <90669192+SayyadSoheb1999@users.noreply.github.com>
2021-09-29 10:37:23 +0530

    boject

---
+++ dbData.js
@@ -0,0 +1,7 @@
+export const dbData={
+    arr:[
+       {firstName:"rahim",MiddleName:"samad",lastName:"shaikh"},
+       {firstName:"mohammad",MiddleName:"shoheb",lastName:"sayyad"},
+       {firstName:"arman",MiddleName:"dipak",lastName:"lakhani"},
+   ]};
+   
