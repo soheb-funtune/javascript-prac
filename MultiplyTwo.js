dcbbc9b7b5f9d61a3a636bd37a095e90780cc888
soheb-funtune <soheb.sayyad@fyntune.com>
2023-06-27 08:27:59 +0530

    multiply 2 num without using *

--- MultiplyTwo.js
+++ MultiplyTwo.js
@@ -1,11 +1,10 @@
+// multiply Two numbers without using * operator
 
-
-function Multi(a,b) {
-    let sum =a;
-    for(i=0;i<b-1;i++){
-        sum += a;
-
-    }return sum;
-
+function Multi(a, b) {
+  let sum = 0;
+  for (i = 1; i <= b; i++) {
+    sum += a;
+  }
+  return sum;
 }
-console.log(Multi(6,6));
+console.log(Multi(8, 5));
